637ea687e5f69cfd0092b65bbcbe5d4eed40d954
Colt King <king.colt@yahoo.ca>
2019-05-06 22:47:43 -0700

    Made a new version of the project without all of the nonsense and broken packages I had before

---
+++ Assets/Scripts/AISystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISystem : MonoBehaviour
+{
+    struct AIComponents
+    {
+        public AIComponent ai;
+        public Transform transform;
+    };
+
+    void Update()
+    {
+        float dTime = Time.deltaTime;
+
+        /*foreach(var entity in GetEntities<AIComponents>())
+        {
+            //do things
+        }*/
+        //find some way to do this without ecs package
+    }
+}
